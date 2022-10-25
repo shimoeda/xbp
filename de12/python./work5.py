364543dd765571bca70bbc1419ed5f361466132c
shimoeda <r202202018fo@jindai.jp>
2022-10-25 15:42:03 +0900

    20221025

--- de12/python./work5.py
+++ de12/python./work5.py
@@ -1,12 +1,14 @@
 for i in range(1,4):
     print(i,"人目")
-name=input("名前を教えて下さい")
-waist=input("腹囲は？")
-age=int(input("年齢は？"))
 
-print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
+    name=input("名前を教えて下さい")
+    waist=input("腹囲は？")
+    age=int(input("年齢は？"))
 
-if waist>=85 and age>=40:
-    print(name,"さん、内臓脂肪蓄積注意です")
-else:
-    print(name,"さん、腹囲は問題ありません")
+    print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
+
+
+    if waist>=85 and age>=40:
+        print(name,"さん、内臓脂肪蓄積注意です")
+    else:
+        print(name,"さん、腹囲は問題ありません")
